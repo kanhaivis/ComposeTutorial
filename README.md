1db8c8bd3472b67dd1ef0fe97faf9931eeacee5e
kanhaivis <kanhaivis@gmail.com>
2024-08-12 12:40:58 +0530

    use text properties

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Compose Tutorial
 
 1.Text in JetPack Compose
+Text is TextView.
+1. Text Size: Text("Title", fontSize=14.sp); 
--- app/src/main/java/com/kanhai/myapplication/MainActivity.kt
+++ app/src/main/java/com/kanhai/myapplication/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.kanhai.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android12345")
+                    Greeting("Android 12345 ")
                 }
             }
         }
@@ -31,16 +36,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    SelectionContainer {
+        MyTextView(name.repeat(50))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("Android ")
+
     }
+}
+
+@Composable
+fun MyTextView(name:String){
+    Text(text = name,
+        fontSize = 18.sp,
+        color = Color.Blue,
+        fontStyle = FontStyle.Italic,
+        maxLines = 4,
+        overflow = TextOverflow.Ellipsis
+    )
 }
